70495cecfc23d7fa18f9e8ae41f9cb77c84700fb
Oleksandr Solodkyi <asolodkiy@ukr.net>
2015-02-09 00:38:27 +0200

    add(structure)

---
+++ app.js
@@ -0,0 +1,29 @@
+(function($scope){
+	'use strict';
+
+
+
+	var app = angular.module('library', []);
+
+	app.controller('AuthorsListController', ['$http', function($http){
+		var persons = this;
+		persons.authors = [];
+		$http.get('./data/authors.json').success(function(data){
+			persons.authors = data;
+		});
+	}]);
+
+	app.controller('BooksListController', ['$http', function($http){
+		var literature = this;
+		literature.books = [];
+		$http.get('./data/books.json').success(function(data){
+			literature.books = data;
+		})
+	}]);
+
+	//app.controller('ContentCtrl', function($scope) {
+	//	//$scope.count = "000";
+	//});
+
+})();
+
